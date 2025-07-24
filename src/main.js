0e66027925fc541af2c61516ba0a7223ea23e9c5
bzk9x <bzk9xx@gmail.com>
2025-07-25 00:50:32 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    frame: false,
+    maximizable: false,
+    resizable: false,
+    transparent: true,
+    show: false,
+    focusable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('src/app/res/layout/clock.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
