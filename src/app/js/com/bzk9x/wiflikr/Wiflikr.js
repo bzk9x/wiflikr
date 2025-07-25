e20e77148c5e87344dfff7f7ca5a3d626a36db01
bzk9x <bzk9xx@gmail.com>
2025-07-25 21:35:31 +0100

    feat(wiflikr): add widget preview with shape selection functionality
    - Implement widget preview with scaling animation on hover
    - Add shape selection functionality with three different widget shapes
    - Update layout and styles to support new preview and editor components
    - Include background image for ambience effect

--- src/app/js/com/bzk9x/wiflikr/Wiflikr.js
+++ src/app/js/com/bzk9x/wiflikr/Wiflikr.js
@@ -1,10 +1,33 @@
-const dragArea = document.getElementById('title-bar-idk')
-const hintContainer = document.getElementById('window-dragable-hint-container')
+window.addEventListener('DOMContentLoaded', () => {
+    const el = document.getElementById('widget-preview')
+    if (!el) return
 
-dragArea.addEventListener('mouseenter', () => {
-    hintContainer.classList.add('active')
-})
+    el.addEventListener('mouseenter', () => {
+        el.classList.add('transitioning', 'scaled')
+        setTimeout(() => el.classList.remove('transitioning'), 500)
+    })
 
-dragArea.addEventListener('mouseleave', () => {
-    hintContainer.classList.remove('active')
+    el.addEventListener('mouseleave', () => {
+        el.classList.remove('scaled', 'transitioning')
+    })
+
+    const widget = document.getElementById('widget');
+    const widgetShape1 = document.getElementById('widget-shape-1');
+    const widgetShape2 = document.getElementById('widget-shape-2');
+    const widgetShape3 = document.getElementById('widget-shape-3');
+
+    widgetShape1.addEventListener('click', () => {
+        widget.classList.add('shape-1');
+        widget.classList.remove('shape-2', 'shape-3');
+    });
+
+    widgetShape2.addEventListener('click', () => {
+        widget.classList.add('shape-2');
+        widget.classList.remove('shape-1','shape-3');
+    });
+
+    widgetShape3.addEventListener('click', () => {
+        widget.classList.add('shape-3');
+        widget.classList.remove('shape-1','shape-2');
+    });
 })
