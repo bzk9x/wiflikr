ce4e230c4414eae9355d603833936770ba5faa07
bzk9x <bzk9xx@gmail.com>
2025-07-25 21:46:37 +0100

    feat(widget): add hover and selection styles for shape widgets
    - Add hover effect with brightness filter to shape widgets
    - Implement visual selection state with border styling
    - Update JavaScript to toggle selected class on click
    - Set first shape as selected by default in HTML

--- src/app/js/com/bzk9x/wiflikr/Wiflikr.js
+++ src/app/js/com/bzk9x/wiflikr/Wiflikr.js
@@ -19,15 +19,24 @@ window.addEventListener('DOMContentLoaded', () => {
     widgetShape1.addEventListener('click', () => {
         widget.classList.add('shape-1');
         widget.classList.remove('shape-2', 'shape-3');
+        widgetShape1.classList.add('selected');
+        widgetShape2.classList.remove('selected');
+        widgetShape3.classList.remove('selected');
     });
 
     widgetShape2.addEventListener('click', () => {
         widget.classList.add('shape-2');
         widget.classList.remove('shape-1','shape-3');
+        widgetShape2.classList.add('selected');
+        widgetShape1.classList.remove('selected');
+        widgetShape3.classList.remove('selected');
     });
 
     widgetShape3.addEventListener('click', () => {
         widget.classList.add('shape-3');
         widget.classList.remove('shape-1','shape-2');
+        widgetShape3.classList.add('selected');
+        widgetShape1.classList.remove('selected');
+        widgetShape2.classList.remove('selected');
     });
 })
